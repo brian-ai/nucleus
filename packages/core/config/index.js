ea1ffdc13db0a59338bf0b1dd068de06dcc5d002
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-05-23 10:03:04 -0300

    feat(config): add time options config

--- packages/core/config/index.js
+++ packages/core/config/index.js
@@ -10,5 +10,9 @@ module.exports = {
   WIT_URL: process.env.WIT_URL,
   WIT_TOKEN: process.env.WIT_TOKEN,
   GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-  DARKSKY_API: process.env.DARKSKY_API
+  DARKSKY_API: process.env.DARKSKY_API,
+  TIME_OPTIONS: {
+    hour: '2-digit',
+    minute: '2-digit'
+  }
 }
